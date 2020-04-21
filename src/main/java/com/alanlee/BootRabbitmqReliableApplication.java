f81019e7cf546ccc42183b5f00c548796eaa751f
AlanLee-Java <19865879289@163.com>
2020-04-21 18:15:45 +0800

    初始化项目。

---
+++ src/main/java/com/alanlee/BootRabbitmqReliableApplication.java
@@ -0,0 +1,13 @@
+package com.alanlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRabbitmqReliableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRabbitmqReliableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alanlee/BootRabbitmqReliableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanlee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRabbitmqReliableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
